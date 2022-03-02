ec4c29fd5254f1704b48087422bbe55045be7a49
saujanyaraj.07 <saujanyaraj.07@gmail.com>
2022-03-02 22:56:02 +0530

    nagation bar added

--- lib/navScreens/navigationScreens.dart
+++ lib/navScreens/navigationScreens.dart
@@ -6,43 +6,174 @@ class Nav_A extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blueAccent,
-        body: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: [
-                Image.network(
-                  'https://images.unsplash.com/photo-1646191933927-c67bb9cb0f66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                  fit: BoxFit.cover,
-                ),
-                SizedBox(
-                  height: 200,
-                ),
-                Text(
-                  "Real-Time AQi Monitoring ",
-                  style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                SizedBox(
-                  height: 100,
-                ),
-                ElevatedButton(
-                  child: Text(
-                    'Next ',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.black,
-                  ),
-                ),
-              ],
+      backgroundColor: Colors.blueAccent,
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/bg.png',
+            fit: BoxFit.cover,
+          ),
+          Container(
+            height: 369,
+            width: 429,
+            child: Image.asset(
+              'assets/images/next1.png',
+              fit: BoxFit.cover,
             ),
           ),
-        ));
+          Positioned(
+            top: 450,
+            left: 40,
+            child: Text(
+              "Real-Time AQI Monitoring",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+          Positioned(
+            left: 130,
+            top: 700,
+            child: MaterialButton(
+              height: 40,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              color: Colors.black,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Nav_B()),
+                );
+              },
+              child: Text(
+                "Next",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Nav_B extends StatelessWidget {
+  const Nav_B({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/bg.png',
+            fit: BoxFit.cover ,
+          ),
+          // Container(
+          //   height: 369, 
+          //   width: 429,
+          //   child: Image.asset(
+          //     'assets/images/next1.png',
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
+          Positioned(
+            top: 450,
+            left: 125,
+            child: Text(
+              "Mitigation",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+          Positioned(
+            left: 130,
+            top: 700,
+            child: MaterialButton(
+              height: 40,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              color: Colors.black,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Nav_C()),
+                );
+              },
+              child: Text(
+                "Next",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Nav_C extends StatelessWidget {
+  const Nav_C({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/bg.png',
+            fit: BoxFit.cover,
+          ),
+          // Container(
+          //   height: 369,
+          //   width: 429,
+          //   child: Image.asset(
+          //     'assets/images/next1.png',
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
+          Positioned(
+            top: 450,
+            left: 125,
+            child: Text(
+              "Mitigation",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+          Positioned(
+            left: 130,
+            top: 700,
+            child: MaterialButton(
+              height: 40,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              color: Colors.black,
+              onPressed: () {},
+              child: Text(
+                "I agree",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
