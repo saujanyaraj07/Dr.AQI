e198bed068093fc4bc75cf747bbb2143075f5e77
saujanyaraj.07 <saujanyaraj.07@gmail.com>
2022-03-05 12:25:53 +0530

    -

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -7,7 +7,7 @@ import '../auth/login_page.dart';
 import '../constants/gradient.dart';
 
 class Splash extends StatefulWidget {
-  const Splash({ Key? key }) : super(key: key);
+  const Splash({Key? key}) : super(key: key);
 
   @override
   State<Splash> createState() => _SplashState();
@@ -17,49 +17,49 @@ class _SplashState extends State<Splash> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 6),
-          ()=>Navigator.pushReplacement(context,
-                MaterialPageRoute(builder:
-                                  (context) => 
-                                  LoginPage()
-                                  )
-                )
-    );
+    Timer(
+        const Duration(seconds: 6),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginPage())));
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      /*decoration: BoxDecoration(
+        color: Colors.white,
+        /*decoration: BoxDecoration(
         gradient:AppGradient(),
       ),*/
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            'images/drone.png',
-            width: MediaQuery.of(context).size.width*0.5,
-            fit: BoxFit.contain,
-          ), 
-          Image.asset(
-            'images/logo-1.png',
-            width: MediaQuery.of(context).size.width*0.7,
-            fit: BoxFit.contain,
-          ), 
-          const SizedBox(height: 100.0,),
-          DefaultTextStyle(
-            style: TextStyle(
-                color: Colors.blue[900],
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'RobotoMono'
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            Image.asset(
+              'assets/images/drone.png',
+              width: MediaQuery.of(context).size.width * 0.5,
+              fit: BoxFit.contain,
+            ),
+            Image.asset(
+              'assets/ images/logo-1.png',
+              width: MediaQuery.of(context).size.width * 0.7,
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(
+              height: 100.0,
+            ),
+            DefaultTextStyle(
+              style: TextStyle(
+                  color: Colors.blue[900],
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoMono'),
+              textAlign: TextAlign.center,
+              child: const Text(
+                'For a Healthier life that we deserve!',
               ),
-            textAlign: TextAlign.center,
-            child: const Text('For a Healthier life that we deserve!',),
-          ),
-        ],
-      )
-    );
+            ),
+          ],
+        ));
   }
 }
